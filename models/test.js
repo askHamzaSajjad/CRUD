286d24e88bba20a97e7f1142ead6cbaa9207854e
Muhammad Hamza Sajjad <askhamzasajjad@gmail.com>
2023-05-15 09:42:51 +0500

    copy branch

--- models/test.js
+++ models/test.js
@@ -1,1 +1,6 @@
 // Something new should be added
+router.post('/register',controller.registerController);            //********// 
+router.post('/login',controller.loginController);                  //********//  
+router.post('/updateitems',controller.updateItemController);       //********// 
+router.post('/deleteitem', controller.deleteItemController);       //********// 
+router.post('/additems', controller.addItemController);   
